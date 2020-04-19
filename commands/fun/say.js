13a8abe02da6408e88f24f5eb9ba4697b94ffe11
Pneuma <syca0714@gmail.com>
2020-04-19 16:30:20 +0900

    Update say.js

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -8,8 +8,6 @@ module.exports = {
   args: true,
   usage: ['[보낼 메시지]'],
   async execute(message, args) {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = args.join(" ");
     
     if (message.channel.type === 'text')
